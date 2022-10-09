3e89dcb9149c64fc8dd975c3045942c1d1ad6f2b
Anamitra Dey <anamitragraduation@gmail.com>
2022-10-09 21:27:20 +0530

    fixed routes

--- routes/restaurant.routes.js
+++ routes/restaurant.routes.js
@@ -2,8 +2,8 @@ const restaurantController= require('../controllers/restaurant.controller')
 
 module.exports= (app) =>{
 
-    app.get('/restaurantapp/api/v1/restaurants',restaurantController.getAllRestaurants);
+    app.get('/restaurantapp/api/v1/restaurant/',restaurantController.getAllRestaurants);
     app.get('/restaurantapp/api/v1/categories',restaurantController.getAllCategories);
     app.get('/restaurantapp/api/v1/categories/:category',restaurantController.getRestaurantByCategory);
-    app.post('/restaurantapp/api/v1/restaurant',restaurantController.createRestaurant);
+    app.post('/restaurantapp/api/v1/restaurant/add',restaurantController.createRestaurant);
 }
